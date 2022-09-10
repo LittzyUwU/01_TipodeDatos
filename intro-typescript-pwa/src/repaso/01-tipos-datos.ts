048adc271991e6da561fdd0bb43d4a59c2f4c0a6
LittzyUwU <gutierrezlittzy@gmail.com>
2022-09-10 09:49:12 -0500

    OvandoGutierrez_01_TipodeDatos

---
+++ intro-typescript-pwa/src/repaso/01-tipos-datos.ts
@@ -0,0 +1,15 @@
+
+
+console.log('Hola Mundo!');
+
+let nombre:string="Litty";
+let cont:number=111;
+let acredita:boolean=false;
+
+let apaterno:number|string='juan';
+
+apaterno=228;
+
+var miVariable:string='Hola';
+const tem:number=234;
+
